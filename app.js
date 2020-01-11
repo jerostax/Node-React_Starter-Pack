898f2e6507c8ce4486dbc256024971c3045c4ba3
Jeremy <jeremy.geneste@gmail.com>
2020-01-11 11:24:53 +0100

    added some comments on the app.js server

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+// * * * Here we import our routes from the sample.js file of the routes directory
 const sampleRoutes = require('./routes/sample');
 
+// * * * Here we add the body parser
 // app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// * * * Here we set the headers to avoid CORS errors
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
@@ -20,8 +23,11 @@ app.use((req, res, next) => {
   next();
 });
 
+// * * * Here we use the routes from the sample.js file in the route directory
+// * * * All routes of this file will go through /sample
 app.use('/sample', sampleRoutes);
 
+// * * * Here is the code to handle paths in production mode
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
   app.use(express.static('client/build'));
