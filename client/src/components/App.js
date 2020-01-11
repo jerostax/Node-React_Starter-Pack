d44d4495fa78e1a016a0b3b730f9332f8f29e5ce
Jeremy <jeremy.geneste@gmail.com>
2020-01-11 10:48:33 +0100

    get sample request from client to server

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import axios from 'axios';
 
 import './app.css';
 
 class App extends React.Component {
+  state = { content: '' };
+
+  getSample = async () => {
+    try {
+      const response = await axios.get('/sample/helloworld');
+      console.log(response.data.message);
+      this.setState({ content: response.data.content });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   render() {
+    let { content } = this.state;
     return (
       <div>
         <h1>Welcome to my React App</h1>
+        <button type='button' onClick={this.getSample}>
+          Get Sample from Server
+        </button>
+        {content ? <p>{content}</p> : ''}
       </div>
     );
   }
