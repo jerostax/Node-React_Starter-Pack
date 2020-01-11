51a1de10aa0c43ec379e37f646049634a32ed5fc
Jeremy <jeremy.geneste@gmail.com>
2020-01-11 09:29:25 +0100

    refacto react client

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './app.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to my React App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
