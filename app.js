7564efb8734f604f2da04b7e5da18ca49c5dfea1
Jeremy <jeremy.geneste@gmail.com>
2020-01-11 10:35:42 +0100

    set headers to avoid CORS errors on server

--- app.js
+++ app.js
@@ -4,11 +4,21 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
 const sampleRoutes = require('./routes/sample');
 
-app.use(sampleRoutes);
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use('/sample', sampleRoutes);
 
 app.listen(PORT);
